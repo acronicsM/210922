874cc5e5de5b730ad12007c1aef44c45b260d13d
MikeParamonov <kloze-spb@mal.ru>
2022-11-16 16:00:59 +0300

    l1

--- frontend/src/components/project.js
+++ frontend/src/components/project.js
@@ -2,7 +2,8 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 
-const ProjectItem = ({project}) =>{
+
+const ProjectItem = ({project,delete_project}) =>{
     return(
         <tr>
             <td>
@@ -13,18 +14,22 @@ const ProjectItem = ({project}) =>{
              <td>
                 <Link to={`/projects/${project.id}`}>task list</Link>
             </td>
+            <td>
+                <button onClick={()=>delete_project(project.id)} type='button'>Delete</button>
+            </td>
         </tr>
     )
 }
 
-const ProjectList = ({projects}) =>{
+const ProjectList = ({projects,delete_project}) =>{
     return(
         <table>
             <th>Project name</th>
             <th>Link</th>
             <th>users</th>
             <th>todo</th>
-            {projects.map((project_) => <ProjectItem project={project_} />)}
+            <th>actions</th>
+            {projects.map((project_) => <ProjectItem project={project_} delete_project={delete_project}/>)}
         </table>
     )
 }

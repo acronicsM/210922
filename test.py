1571eb0510b88023decf07d18e3e04da0de0e9d3
MikeParamonov <kloze-spb@mal.ru>
2022-11-17 15:18:07 +0300

    L12

--- test.py
+++
@@ -1,4 +0,0 @@
-import requests
-response = requests.post('http://127.0.0.1:8000/api-token-auth/', data={'username':'admin1@admin1.com', 'password': 'admin1'})
-print(response.status_code)
-print(response.json()) 
